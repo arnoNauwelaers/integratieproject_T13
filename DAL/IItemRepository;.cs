33b8da6875458687dc75df150cad8045d391c4d1
Yago De Cuyper <yago.decuyper@student.kdg.be>
2018-03-27 14:23:11 +0200

    IItemrepo, theme public gemaakt

---
+++ DAL/IItemRepository;.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Domain;
+
+namespace DAL
+{
+  public interface IItemRepository_
+  {
+    void CreatePerson(Person person);
+    void CreateOrganization(Organization organization);
+    void CreateTheme(Theme theme);
+    Person ReadPerson(int id);
+    Organization ReadOrganization(int id);
+    Theme ReadTheme(int id);
+    void UpdatePerson(Person person);
+    void UpdateOrganization(Organization organisation);
+    void UpdateTheme(Theme theme);
+    void DeletePerson(Person person);
+    void DeleteOrganization(Organization organization);
+    void DeleteTheme(Theme theme);
+  }
+}
--- Domain/Theme.cs
+++ Domain/Theme.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BL.Domain
 {
-    class Theme
+    public class Theme
     {
         public ICollection<string> Keywords { get; set; }
     }
