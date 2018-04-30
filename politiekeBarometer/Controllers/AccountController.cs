b1102e3acf22fbf8613790100a33f4731ab1b3fc
Mathijs333 <mathijs.constantin@student.kdg.be>
2018-04-30 12:45:05 +0200

    Email register fix

--- politiekeBarometer/Controllers/AccountController.cs
+++ politiekeBarometer/Controllers/AccountController.cs
@@ -81,7 +81,7 @@ namespace politiekeBarometer.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.UserName };
+                var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
